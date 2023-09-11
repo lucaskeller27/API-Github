cf166a161028b896937d3111333e4f7c4cbab0f1
LUCAS KELLER DA SILVA <lucas_keller-silva@estudante.sesisenai.org.br>
2023-09-11 12:35:21 -0300

    finished

--- script.js
+++ script.js
@@ -1,34 +1,23 @@
-const userName = 'seu_username';
-const apiUrl = `https://api.github.com/users/${userName}`;
-
-async function fetchGitHubUserData() {
-  try {
-    const response = await fetch(apiUrl);
-    if (!response.ok){
-      throw new Error(`Erro na solitação: ${response.statusText}`);
-    }
-   
-    const userData = await response.json();
-
-      const pfp = userData.avatar_url;
-      const nome = userData.name;
-      const username = userData.login;
-      const repos = userData.public_repos;
-      const seguidores = userData.followers;
-      const seguindo = userData.following;
-      const reposUrl = userData.repos_url;
-
-    console.log('Imagem do Avatar:', pfp);
-    console.log('Nome:', nome);
-    console.log('Username:', username);
-    console.log('Quantidade de Repositórios:', repos);
-    console.log('Quantidade de Seguidores:', seguidores);
-    console.log('Quantidade de Usuários Seguindo:', seguindo);
-    console.log('Link para Repositórios:', reposUrl);
-
-  } catch (error) {
-    console.error('Ocorreu um erro:', error);
+function getProfile() {
+    event.preventDefault();
+    const username = document.getElementById('usernameInput').value;
+    fetch(`https://api.github.com/users/${username}`)
+      .then(res => res.json())
+      .then(data => {
+        const profile = document.getElementById('profile-info');
+        if (data.message === 'Not Found') {
+          profile.innerHTML = "No profile";
+        } else {
+          profile.innerHTML = `
+            <img src="${data.avatar_url}"><br> 
+            <span id="OutData">Nome: </span>${data.name}<br>
+            <span id="OutData">Nome de usuário: </span> ${data.login}<br>
+            <span id="OutData">Repositórios: </span> ${data.public_repos}<br>
+            <span id="OutData">Seguidores: </span> ${data.followers}<br>
+            <span id="OutData">Seguindo: </span> ${data.following}<br>
+            <span id="OutData">Link do perfil: </span> ${data.html_url+'/?tab=repositories'}<br>
+          `;
+        }
+      });
   }
-}
-
-fetchGitHubUserData();
+  
